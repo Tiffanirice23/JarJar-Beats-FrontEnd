b2e546368366b812c2e2ca74c5175ec6d68b2ab6
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-05-31 13:51:14 -0400

    links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import AboutUs from './AboutUs';
+import Search from './Search';
+import Playlist from './Playlist';
+import Artist from './Artist';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+
+        </header>
+
+        <Router>
+          <Routes>
+
+            <Route
+              //  is the homepage
+              exact path="/"
+              //the homepage will render bestbooks js
+              element={<Search />}
+            > </Route>
+
+            <Route
+              //  is the homepage
+              path="/artist"
+              //the homepage will render bestbooks js
+              element={<Artist />}
+            > </Route>
+
+            <Route
+              //  is the homepage
+              path="/playlist"
+              //the homepage will render bestbooks js
+              element={<Playlist />}
+            > </Route>
+
+            <Route
+              //  is the homepage
+              path="/about-us"
+              //the homepage will render bestbooks js
+              element={<AboutUs />}
+            > </Route>
+
+          </Routes>
+
+        </Router>
+
+      </div>
+    );
+  }
 }
+
 
 export default App;
