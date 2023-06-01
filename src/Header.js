5f4d30fbaa7b4aafeb445ab1a31a532b13c81ce3
AnthonySinitsa <itonyemail@gmail.com>
2023-06-01 16:13:46 +0000

    asdf

--- src/Header.js
+++ src/Header.js
@@ -4,20 +4,20 @@ import { Link } from "react-router-dom";
 import './Header.css'
 
 class Header extends React.Component {
-  render() {
-    return (
-      <>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Navbar.Brand>JarJar beats cult, please take a drink, it won't hurt you we swear</Navbar.Brand>
-          {/* These creates a button to specific links */}
-          <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
-          <NavItem><Link to="/artist" className="nav-link">artist</Link></NavItem>
-          <NavItem><Link to="/playlist" className="nav-link">playlist</Link></NavItem>
-          <NavItem><Link to="/about-us" className="nav-link">about-us</Link></NavItem>
-        </Navbar>
-      </>
-    )
-  }
+    render() {
+        return (
+            <>
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Navbar.Brand>JarJar beats cult, please take a drink, it won't hurt you we swear</Navbar.Brand>
+                    {/* These creates a button to specific links */}
+                    <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
+                    {/* <NavItem><Link to="/artist" className="nav-link">artist</Link></NavItem> */}
+                    <NavItem><Link to="/playlist" className="nav-link">playlist</Link></NavItem>
+                    <NavItem><Link to="/about-us" className="nav-link">about-us</Link></NavItem>
+                </Navbar>
+            </>
+        )
+    }
 }
 
 export default Header;
--- src/Playlist.js
+++ src/Playlist.js
@@ -3,37 +3,37 @@ import axios from 'axios';
 import { withAuth0 } from "@auth0/auth0-react";
 
 class Playlist extends React.Component {
-  //if you send the email address as query with this request it will return only the playlists that were created by the user that logged in. 
-  getPlaylist = async () => {
-    try {
-      if (this.props.auth0.isAuthenticated) {
-        const res = await this.props.auth0.getIdTokenClaims();
-        const jwt = res.__raw;
-        const config = {
-          method: 'get',
-          baseURL: process.env.REACT_APP_SERVER,
-          url: '/playlist',
-          headers: { "Authorization": `Bearer ${jwt}` }
-        }
-        const results = await axios(config);
-        console.log(results.data);
-      }
-    } catch (err) {
-      console.log("nay nay", err.response);
-    }
-  }
-  componentDidMount() {
-    this.getPlaylist();
-  }
+    //if you send the email address as query with this request it will return only the playlists that were created by the user that logged in. 
+    getPlaylist = async () => {
+        try {
+            if (this.props.auth0.isAuthenticated) {
+                const res = await this.props.auth0.getIdTokenClaims();
+                const jwt = res.__raw;
+                const config = {
+                    method: 'get',
+                    baseURL: process.env.REACT_APP_SERVER,
+                    url: '/playlist',
+                    headers: { "Authorization": `Bearer ${jwt}` }
 
-  render() {
-    console.log(this.props.auth0.user);
-    return (
-      <>
-        <p> Playlist </p>
-      </>
-    )
-  }
+                }
+                const results = await axios(config);
+                console.log(results.data);
+            }
+        } catch (err) {
+            console.log("nay nay", err.response);
+        }
+    }
+    componentDidMount() {
+        this.getPlaylist();
+    }
+    render() {
+        console.log(this.props.auth0.user);
+        return (
+            <>
+                <p> Playlist </p>
+            </>
+        )
+    }
 }
 
 export default withAuth0(Playlist);
