cd480dd4a8db479c3e194a5ec00465faa2844881
Daniel Quinn <dandidit.quinn@gmail.com>
2023-06-01 21:08:00 -0700

    cssStuff

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,8 @@ import Navbar from 'react-bootstrap/Navbar';
 class Footer extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>Code Fellows</Navbar.Brand>
+      <Navbar className='footer' collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand>&copy; Dan Quinn</Navbar.Brand>
       </Navbar>
     )
   }
--- src/Profile.js
+++ src/Profile.js
@@ -12,7 +12,7 @@ const Profile = () => {
     isAuthenticated && (
       <div>
         {/* <img src={user.picture} alt={user.name} /> */}
-        <h2>{user.name}</h2>
+        <h2 className="navProfile">{user.name}</h2>
         {/* <p>{user.email}</p> */}
       </div>
     )
